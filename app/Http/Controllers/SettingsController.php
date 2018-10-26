8b1f43906d9d58be64d4ed4a1b496f2ec69b70c1
harley-travis <harley.travis2@gmail.com>
2018-10-25 23:28:35 -0600

    settings file

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class SettingsController extends Controller
 {
-    //
+     // get data for page
+     public function index() {
+        return view('settings.overview');
+    }
+    // upload user image
+    
+    // change password
 }
